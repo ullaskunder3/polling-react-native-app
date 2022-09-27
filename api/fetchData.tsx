ab742fdef598520d05d5e03c31fee72b3f1ec7db
ullas <ullaskunder3@gmail.com>
2022-09-27 15:02:47 +0530

    table task done

---
+++ api/fetchData.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+async function fetchPollingData(pageNumber:number) {
+    
+    try {
+        const response = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`);           
+        return response.data;
+    } catch (error) {
+        if(axios.isAxiosError(error)){
+            console.log("ERROR:check axios:", error.message);
+            return '404';
+        }else{
+            console.log("ERROR: something else", error);
+            return "404"
+        }
+    }
+}
+
+export default fetchPollingData;
---
+++ screen/Home.tsx
@@ -0,0 +1,136 @@
+import { View, Text, FlatList, StyleSheet, Linking } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import fetchPollingData from '../api/fetchData';
+const smallScreenIndices = [0, 1, 5];
+const mediumScreenIndices = [0, 1, 2, 4, 5];
+
+export const Home = () => {
+    const [data, setData] = useState<object[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [currentPage, setCurrentPage] = useState<number>(0);
+
+    useEffect(() => {
+        fetchPollingData(0)
+            .then(res => {
+                return res.hits;
+            })
+            .then(body => setData([...body]))
+            .catch(error => console.log(error))
+    }, [])
+    const tableHading = ["Title", "Url", "Created At", "Auther"]
+
+    const createShortString = (str: string) => {
+        if (str != undefined) {
+            // return str.length < 40 ? str : str.substring(0, 50) + '...'
+            return str.length < 5 ? str : str.substring(0, 15) + '...'
+        }
+    }
+    const parseDateStamp = (created_at: string) => {
+        const dateOptions: any = { timeZone: 'UTC', month: 'long', day: 'numeric', year: 'numeric' };
+        const myDate = new Date(created_at);
+
+        return myDate.toLocaleDateString('en-US', dateOptions);
+    }
+
+    const renderItem = ({ item }) => {
+        const { title, url, created_at, author } = item
+
+        const newShortTitle = createShortString(title)
+        const urlDomain = url?.split('/')[2];
+        const newShorAuther = createShortString(author)
+
+        const newCreatedAt = parseDateStamp(created_at);
+
+        return (
+            <View style={styles.tableContent}>
+                <View style={[styles.tableCol, styles.tableFirstCol]}>
+                    <Text>{newShortTitle}</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text
+                        style={styles.tableContentLink}
+                        onPress={() => Linking.openURL(url)}>
+                        {urlDomain?urlDomain:'unavailable'}
+                    </Text>
+                </View>
+                <View style={[styles.tableCol, { alignItems: 'center' }]}>
+                    <Text>{newCreatedAt}</Text>
+                </View>
+                <View style={[styles.tableCol, styles.tableLastCol]}>
+                    <Text>{newShorAuther}</Text>
+                </View>
+            </View>
+        )
+    }
+
+    // title, url, created_at, auther
+    return (
+        <View style={styles.tableContainer}>
+
+            <View style={styles.tableHeading}>
+                <View style={[styles.tableCol, styles.tableFirstCol]}>
+                    <Text>Title</Text>
+                </View>
+                <View style={styles.tableCol}>
+                    <Text style={styles.tableContentLink}>
+                        Url
+                    </Text>
+                </View>
+                <View style={[styles.tableCol, { alignItems: 'center' }]}>
+                    <Text>Created At</Text>
+                </View>
+                <View style={[styles.tableCol, styles.tableLastCol]}>
+                    <Text>Author</Text>
+                </View>
+            </View>
+
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+            />
+
+
+
+            <StatusBar style="auto" hidden />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    tableContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    tableHeading: {
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderBottomColor: '#021847',
+    },
+    tableContent: {
+        flexDirection: 'row',
+        backgroundColor: '#f3f3f3'
+    },
+    tableContentLink: {
+        color: 'blue',
+        fontSize: 10,
+        textAlign: 'center'
+    },
+    tableCol: {
+        width: 100,
+        paddingVertical: 10,
+        paddingHorizontal: 2,
+        borderRightColor: '#55555585',
+        borderRightWidth: .5
+    },
+    tableFirstCol: {
+        paddingLeft: 0,
+        paddingRight: 5,
+    },
+    tableLastCol: {
+        paddingRight: 0,
+        paddingLeft: 5,
+        borderRightColor: undefined,
+        borderRightWidth: 0
+    }
+})
